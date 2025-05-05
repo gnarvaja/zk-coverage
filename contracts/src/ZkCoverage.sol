6b5ba218e7c57b977299240d20b6e2c1dd7f0dc4
Otto Köster <koster.d.otto@gmail.com>
2025-05-05 03:04:16 -0300

    Add contracts and test suite

--- contracts/src/ZkCoverage.sol
+++ contracts/src/ZkCoverage.sol
@@ -6,39 +6,49 @@ import {IVerifier} from "./interfaces/IVerifier.sol";
 import {Policy} from "@ensuro/Policy.sol";
 import {TrustfulRiskModule} from "@ensuro/TrustfulRiskModule.sol";
 
-struct PolicyData {
-    uint256 insuredAmount;
-    bytes32 userLocationHash;
-    bytes32 riskBucket;
-}
-
+// TODO Move this to interfaces
 struct EventData {
     bytes32 root;
     uint256 timestamp;
 }
 
-contract Coverage {
+contract ZkCoverage {
     IVerifier public acquisitionVerifier;
     IVerifier public claimVerifier;
     TrustfulRiskModule public riskModule;
     uint96 internal internalId;
-    mapping(bytes32 => EventData) public events;
-    bytes32 public priceRoot; // TODO Add setter to this
+    uint256 public eventId;
+    mapping(uint256 => EventData) public events;
+    bytes32 public priceRoot;
 
     error InvalidProof();
     error EventNotTriggered(uint256 timestamp);
 
     constructor(
-        address _riskModule,
+        //address _riskModule,
         address _acquisitionVerifier,
         address _claimVerifier
     ) {
-        riskModule = TrustfulRiskModule(_riskModule);
+        //riskModule = TrustfulRiskModule(_riskModule);
         acquisitionVerifier = IVerifier(_acquisitionVerifier);
         claimVerifier = IVerifier(_claimVerifier);
     }
 
-    function pricePolicy(uint256 insuredValue, bytes32 riskBucket) internal returns (uint256 payout, uint256 premium, uint256 lossProb) {
+    // TODO Add access control to this
+    function setPriceRoot(bytes32 _priceRoot) external {
+        priceRoot = _priceRoot;
+    }
+
+    // TODO Add access control to this
+    function registerEvent(
+        bytes32 root,
+        uint256 timestamp
+    ) external {
+        events[eventId] = EventData(root, timestamp);
+        eventId++;
+    }
+
+    function pricePolicy(uint256 insuredValue, uint256 riskBucket) internal returns (uint256 payout, uint256 premium, uint256 lossProb) {
         // TODO Implement this
     }
 
@@ -49,8 +59,8 @@ contract Coverage {
     function newPolicy(
         uint256 insuredValue,
         bytes32 userLocationHash,
-        bytes32 riskBucket,
-        uint64 riskArea,
+        uint256 riskBucket,
+        bytes32 riskArea,
         uint40 expiration,
         address onBehalfOf,
         bytes calldata proof
@@ -58,39 +68,40 @@ contract Coverage {
         bytes32[] memory publicInputs = new bytes32[](4);
         publicInputs[0] = userLocationHash;
         publicInputs[1] = priceRoot;
-        publicInputs[2] = riskBucket;
-        publicInputs[3] = bytes32(uint256(riskArea));
+        publicInputs[2] = bytes32(uint256(riskBucket));
+        publicInputs[3] = riskArea;
         if (!acquisitionVerifier.verify(proof, publicInputs)) {
-            revert InvalidProof();
-        }
+            revert InvalidProof(); 
+        } // TODO Check when it returns false instead of reverting
 
         (uint256 payout, uint256 premium, uint256 lossProb) = pricePolicy(insuredValue, riskBucket);
 
         uint96 id = internalId++;
-        riskModule.newPolicy(payout, premium, lossProb, expiration, onBehalfOf, id);
+        //riskModule.newPolicy(payout, premium, lossProb, expiration, onBehalfOf, id);
     }
 
     function claim(
-        Policy.PolicyData calldata policyData,
+        //Policy.PolicyData calldata policyData,
         bytes32 userLocationHash,
         uint256 severity,
-        bytes32 eventHash,
+        uint256 eventIdentifier,
         bytes calldata proof
     ) external {
-        EventData memory eventData = events[eventHash];
+        EventData memory eventData = events[eventIdentifier];
         if (eventData.timestamp == 0) {
-            revert EventNotTriggered(eventData.timestamp); //TODO Change this
+            revert EventNotTriggered(eventData.timestamp);
         }
+        // TODO Check if the event happened before the policy expiration
 
-        // TODO Need to constrain severity also
-        bytes32[] memory publicInputs = new bytes32[](2);
+        bytes32[] memory publicInputs = new bytes32[](3);
         publicInputs[0] = userLocationHash;
         publicInputs[1] = eventData.root;
+        publicInputs[2] = bytes32(uint256(severity));
         if (!claimVerifier.verify(proof, publicInputs)) {
-            revert InvalidProof();
-        }
+            revert InvalidProof(); 
+        } // TODO Check when it returns false instead of reverting
 
-        uint256 payout = computePayout(policyData.payout, severity);
-        riskModule.resolvePolicy(policyData, payout);
+        //uint256 payout = computePayout(policyData.payout, severity);
+        //riskModule.resolvePolicy(policyData, payout);
     }
 }
